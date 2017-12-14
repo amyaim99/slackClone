7893710ed43494240a304f2a3ed77d1f06a3ac39
amyaim <amyaim99@gmail.com>
2017-12-13 19:38:41 -0500

    saved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  pageTitle = 'Slack Clone';
+  messages: string[] = ['How are you', 'I am tired', 'Alost there!'];
 }
--- src/services/message-data.service.spec.ts
+++ src/services/message-data.service.spec.ts
@@ -13,11 +13,32 @@ describe('MessageDataService', () => {
     expect(service).toBeTruthy();
   }));
 
-  describe('addMessage()', () => {
+  describe('add message ', () => {
 it('should add message to MessageDataSevice', inject([MessageDataService], (service: MessageDataService) => {
-  const message1 = new Message({message: 'Wake up'});
+  const message1 = new Message({ message: 'How was your day' });
   service.addMessage(message1);
-  expect(service.getMessageById(1)).toEqual('Wake up');
+  expect(service.getMessageById(1)).toEqual(message1);
 }));
+});
+
+describe('delete message ', () => {
+  it('should delete message from MessageDataSevice', inject([MessageDataService], (service: MessageDataService) => {
+    const message1 = new Message({ message: 'Another busy day' });
+    const message2 = new Message({ message: 'I am doing okay'});
+    service.addMessage(message1);
+    service.addMessage(message2);
+    service.deleteMessageById(1);
+    expect(service.messages.length === 1);
+  }));
+  });
+  describe('get all messages message ', () => {
+    it('should return all message in MessageDataSevice', inject([MessageDataService], (service: MessageDataService) => {
+      const message1 = new Message({ message: 'Amazing  day', id: 1 });
+      const message2 = new Message({ message: 'I am hmmm', id: 1 });
+      service.addMessage(message1);
+      service.addMessage(message2);
+      expect(service.getAllMessages()).toEqual([message1, message2]);
+    }));
+
 });
 });
